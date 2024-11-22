05474c8065d09955e217bae0cd5d67538781f3cb
Ali Mohamed <Alimohamed2019@gmail.com>
2024-11-22 19:29:52 +0200

    Initial Commit

---
+++ Products Demo/Products_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Products_DemoApp.swift
+//  Products Demo
+//
+//  Created by Ali Mohamed on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Products_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
